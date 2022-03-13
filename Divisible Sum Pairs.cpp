d316a02f51cb3e404c4cf0aec98ceb02daebea14
Indominous1 <93007035+Indominous1@users.noreply.github.com>
2022-03-14 02:10:53 +0530

    Create Divisible Sum Pairs.cpp

---
+++ Divisible Sum Pairs.cpp
@@ -0,0 +1,71 @@
+Problem Statement-
+Given an array of integers and a positive integer k, determine the number of (i,j) pairs where i<j and ar[i] + ar[j] is divisible by k.
+
+Example:
+ar[]={1,2,3,4,5,6}
+k=5
+
+Three pairs meet the criteria:[1,4], [2,3] and [4,6].
+
+Function Description:
+Complete the divisibleSumPairs function in the editor below.
+
+divisibleSumPairs has the following parameter(s):
+->int n: the length of array ar
+->int ar[n]: an array of integers
+->int k: the integer divisor
+
+Returns:
+- int: the number of pairs
+
+Input Format:
+The first line contains 2 space-separated integers, n and k.
+The second line contains n space-separated integers, each a value of ar[i].
+
+Constraints:
+2<=n<=100
+1<=k<=100
+1<=ar[i]<=100
+
+Sample Input:
+STDIN           Function
+-----           --------
+6 3             n = 6, k = 3
+1 3 2 6 1 2     ar = [1, 3, 2, 6, 1, 2]
+Sample Output:
+ 5
+Explanation:
+Here are the 5 valid pairs when k=3:
+->(0,2):ar[0]+ar[2]=1+2=3
+->(0,5):ar[0]+ar[5]=1+2=3
+->(1,3):ar[1]+ar[3]=3+6=9
+->(2,4):ar[2]+ar[4]=2+1=3
+->(4,5):ar[4]+ar[5]=1+2=3
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Soluion-
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k,count=0;
+    cin>>n>>k;
+    int arr[n],sum;
+    for (int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        for (int j=i+1;j<n;j++)
+        {
+            sum=arr[i]+arr[j];
+            if(sum%k==0)
+            {
+                count++;
+            }
+        }
+    }
+    cout<<count;
+    return 0;
+}
