f8cabc1ca42283f59bc3f96060ffd8265f284c87
Indominous1 <93007035+Indominous1@users.noreply.github.com>
2022-08-03 23:24:15 +0530

    Update A Very Big Sum.cpp

--- A Very Big Sum.cpp
+++ A Very Big Sum.cpp
@@ -16,22 +16,22 @@ Input Format-
 The first line of the input consists of an integer n.
 The next line contains n space-separated integers contained in the array.
 
-Output Format
+Output Format-
 
 Return the integer sum of the elements in the array.
 
-Constraints
+Constraints-
 1<=n<=10
 0<=ar[i]<=10^10
 
-Sample Input
-
+Sample Input-
 5
 1000000001 1000000002 1000000003 1000000004 1000000005
-Output
-
+ 
+Output-
 5000000015
 
+ ------------------------------------------------------------------------------------------------------------------------------------------------------------------
 Solution-
  
 #include <bits/stdc++.h>
